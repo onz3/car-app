63563c5028a1cd28338bcffad1f1d6a730afeb1b
Emerson <onz3dev@gmail.com>
2022-01-21 09:06:57 -0300

    now I'm going to code frontend

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,13 +1,47 @@
 class CarsController < ApplicationController
+  before_action :current_car, only: [:show, :edit, :update, :destroy]
+
   def index
+    @cars = Car.all
   end
 
   def show
+
   end
 
   def new
+    @cars = Car.new
   end
 
   def edit
+    
   end
+
+  def create
+    car = Car.create(car_params)
+    redirect_to cars_path
+  end
+
+  def update
+    @cars = Car.find(params[:id])
+    @cars.update(car_params)
+
+    redirect_to car_path(@cars)
+  end
+
+  
+  def destroy
+    @cars = Car.find(params[:id])
+    @cars.destroy
+
+    redirect_to cars_path
+  end
+
+
+  private
+
+  def car_params
+    params.require(:car).permit(:name, :year)
+  end
+
 end
