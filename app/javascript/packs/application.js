9cfae337d25be2d661fbeb6db7ccd0dad5a1b3bd
Emerson <onz3dev@gmail.com>
2022-01-25 11:18:03 -0300

    oi

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
 import "../stylesheets/application"
+import "packs/bootstrap"
 
 Rails.start()
 Turbolinks.start()
